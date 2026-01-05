6541c80e3613882784b499fdecdca84ccc5443d6
bryannaranjo <65364707+bryannaranjo@users.noreply.github.com>
2026-01-05 16:57:31 -0500

    Update install-ohmyzsh.sh

--- install-ohmyzsh.sh
+++ install-ohmyzsh.sh
@@ -3,18 +3,19 @@ sudo apt install zsh -y
 sudo apt install -y zoxide eza bat fzf ripgrep thefuck 
 #tldr
 
+#install ohmyzsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+#make default
+chsh -s $(which zsh)
+#######################################
+# RUN BELOW SEPARATE
+
 # Install Plugins
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 # 1. Download Autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 # 2. Download Syntax Highlighting
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
-#install ohmyzsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-#make default
-chsh -s $(which zsh)
 
 # config
 batcat << 'EOF' >> ~/.zshrc
@@ -36,6 +37,7 @@ sed -i 's/^[[:space:]]*ZSH_THEME=.*/ZSH_THEME="steeef"/' ~/.zshrc
 # ZSH_THEME_RANDOM_CANDIDATES=( "jonathan" "steeef" "linuxonly" "essembeh" "tjkirch" "duellj" "kiwi" "mikeh" "fino" "fox" "funky" "muse" "gnzh" )
 
 sed -i 's/^[[:space:]]*ZSH_THEME_RANDOM_CANDIDATES=.*/ZSH_THEME_RANDOM_CANDIDATES=( "jonathan" "steeef" "agnoster" "essembeh" "tjkirch" "duellj" "kiwi" "mikeh" "fino" "fox" "funky" "muse" "gnzh")/' ~/.zshrc
+
 
 
 
