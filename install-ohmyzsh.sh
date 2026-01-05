56ed748dec7635db0242530322213770b18ab41a
bryannaranjo <65364707+bryannaranjo@users.noreply.github.com>
2026-01-05 10:33:48 -0500

    Update install-ohmyzsh.sh

--- install-ohmyzsh.sh
+++ install-ohmyzsh.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 sudo apt update
@@ -39,5 +38,6 @@ sed -i 's/^[[:space:]]*ZSH_THEME=.*/ZSH_THEME="steeef"/' ~/.zshrc
 # ZSH_THEME_RANDOM_CANDIDATES=( "jonathan" "steeef" "linuxonly" "essembeh" "tjkirch" "duellj" "kiwi" "mikeh" "fino" "fox" "funky" "muse" "gnzh" )
 
 sed -i 's/^[[:space:]]*ZSH_THEME_RANDOM_CANDIDATES=.*/ZSH_THEME_RANDOM_CANDIDATES=( "jonathan" "steeef" "agnoster" "essembeh" "tjkirch" "duellj" "kiwi" "mikeh" "fino" "fox" "funky" "muse" "gnzh")/' ~/.zshrc
+
 
 
